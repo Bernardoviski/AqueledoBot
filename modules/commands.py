2a60d6ec7b90012c772b4d4bbc5d003501c24108
Bernard <joao_Bernardi@outlook.com>
2019-07-23 13:09:48 -0300

    Ping Command Emoji Fix

--- modules/commands.py
+++ modules/commands.py
@@ -13,7 +13,7 @@ class Commands(Cog):
 	@command(name="ping", aliases=["online"])
 	async def online(self, ctx):
 		print("Recieved on ping")
-		await ctx.send(f"ðŸ“ Pong  `{int(round(self.bot.latency*1000, 2))} ms`")
+		await ctx.send(f"🏓 Pong  `{int(round(self.bot.latency*1000, 2))} ms`")
 		print("Ping Done")
 
 	@command(name='level')
